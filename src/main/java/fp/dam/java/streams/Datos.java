3a0a4a2f455e55770f3a762611f4f8204e7065e9
julio <julio@void.jplb.org>
2026-03-11 16:45:06 +0100

    Actualización

--- src/main/java/fp/dam/java/streams/Datos.java
+++ src/main/java/fp/dam/java/streams/Datos.java
@@ -1,10 +1,19 @@
 package fp.dam.java.streams;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Stream;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import fp.dam.java.streams.libros.Libro;
 
 public class Datos {
 	
@@ -14,7 +23,7 @@ public class Datos {
 	 */
 	static Stream<String> getPalabras() {
 		try {
-			Path path = Path.of(BloqueA.class.getResource("/palabras.txt").toURI());
+			Path path = Path.of(Datos.class.getResource("/palabras.txt").toURI());
 			return Files.lines(path);
 		} catch (URISyntaxException | IOException e) {
 			throw new RuntimeException(e);
@@ -27,10 +36,22 @@ public class Datos {
 	 */
 	static Stream<String> getLineas() {
 		try {
-			Path path = Path.of(BloqueB.class.getResource("/La Biblioteca de Babel.txt").toURI());
+			Path path = Path.of(Datos.class.getResource("/La Biblioteca de Babel.txt").toURI());
 			return Files.lines(path);
 		} catch (URISyntaxException | IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	/* -----------------------------------------------------------------------------------------------------------------
+	 *   Retorna una lista de libros almacenada en formato JSON en el fichero "Libros.json"
+	 *  ----------------------------------------------------------------------------------------------------------------
+	 */
+	static List<Libro> getLibros() {
+		Gson gson = new Gson();
+		return gson.fromJson(
+				new BufferedReader(new InputStreamReader(Datos.class.getResourceAsStream("/libros.json"))),
+				new TypeToken<LinkedList<Libro>>(){}.getType());
+	}
+	
 }
